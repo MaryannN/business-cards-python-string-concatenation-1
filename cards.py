54194fbcc7481d5c11ef1c39fcc6c437a93628cb
Maryann Nwude <44924494+MaryannN@users.noreply.github.com>
2022-07-25 17:04:53 -0400

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,17 @@
+name = input("What is your name? ")
+company = input("What company do you work at? ")
+job = input("What is your position? ")
+phone_number = input("What is your phone number? ")
+email = input("What is your email? ")
+website= input("What is your website? ")
+
+print("\n---------------------------")
+print(f'Name: {name}\n\tCompany: {company}\n\tPosition: {job}\n\nPhone Number: {phone_number}\nEmail: {email}\nWebsite: {website}')
+print("---------------------------")
+
+
+# Additional challenges:
+# Tab in the phone number and position elements.
+# Try to split the motto into multiple lines if it's longer than the bars at the top and bottom of the card.
+# Try to make the name centered. There are various ways to do this, and none of them are easy - most involve using the len() function somewhere.
+# https://github.com/upperlinecode/business-cards-python-string-concatenation 
